722756a75e65c8f77794eb96873b1e57e2e16b2d
TatarStefan <40664534+TatarStefan@users.noreply.github.com>
2022-02-26 19:01:23 +0200

    Add files via upload

---
+++ savetheanimals/index.php
@@ -0,0 +1,40 @@
+<?php
+    include_once "database/database.php";
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Zen+Kaku+Gothic+Antique:wght@300&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Save The Animals!</title>
+</head>
+<body>
+    <div class="header">
+
+    </div>
+    <div class="header-image">
+            <img class="header-image-css" src="images/background.jpg">
+    </div>
+
+    <div class="content">
+        <div class="content-news">
+            <h2 class="news-content-text">News</h2>
+            <center>
+                <div class="row-1">
+
+                </div>
+                <div class="row-2">
+
+                </div>
+            </center>
+        </div>
+    </div>
+</body>
+</html>
